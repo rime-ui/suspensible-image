209fa32224c06603dc1adac38383e753a6c0bab2
rimex3 <faresahmed51857@gmail.com>
2025-04-29 09:30:56 +0300

    fix: update Skeleton component to use w-fit class for width

--- src/Skeleton.tsx
+++ src/Skeleton.tsx
@@ -3,7 +3,7 @@ import "./styles.css"
 export default function Skeleton({ className = "" }: { className: string }) {
     return (
         <div
-            className={`relative overflow-hidden bg-zinc-200 w-full ${className}`}
+            className={`relative overflow-hidden bg-zinc-200 w-fit ${className}`}
         >
             <div className="shimmer-overlay" />
         </div >
